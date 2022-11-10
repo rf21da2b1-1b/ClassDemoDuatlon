118111f62b9cc66b428f9817e65b1848728abd89
Peter Levinsky <pele@zealand.dk>
2022-11-10 11:29:10 +0100

    Add project files.

---
+++ ClassDemoDuatlonLib/model/Duathlete.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassDemoDuatlonLib.model
+{
+    public class Duathlete
+    {
+        /*
+         * Der er to properties med constraints Name, AgeGroup
+         */
+        
+        // properties
+        public String Name { get; set; }
+
+        public int AgeGroup { get; set; }
+
+        // Tjek betingelser (Constraints)
+        public static bool CheckName(String name)
+        {
+            if (name is null || name.Trim().Length < 4)
+            {
+                throw new ArgumentException("name skal være mindst 4 tegn langt");
+            }
+
+            return true;
+        }
+        public static bool CheckAgeGroup(int ageGroup)
+        {
+            if (ageGroup < 1 || 4 < ageGroup)
+            {
+                throw new ArgumentException("Der findes kun grupperne 1,2,3 og 4");
+            }
+
+            return true;
+
+        }
+
+        // validerer et helt objekt
+        public bool Validate()
+        {
+            bool ok = true;
+            try
+            {
+                CheckName(Name);
+                CheckAgeGroup(AgeGroup);
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException(e.Message); // eller bare throw;
+            }
+            return ok;
+        }
+
+        /*
+         * Der er tre properties uden constraints
+         */
+        public int Bib { get; set; }
+        public int Bike { get; set; }
+        public int Run { get; set; }
+
+        /*
+         * Begregnet Property Total
+         */
+
+        public int Total // sum Bike + Run
+        { 
+            get
+            {
+                return Bike + Run;
+            }
+         }
+
+
+
+
+        /*
+         * Konstruktør
+         */
+        public Duathlete() : this("dummy", 1, -1, -1, -1)
+        {
+
+        }
+
+        public Duathlete(string name, int ageGroup, int bib, int bike, int run)
+        {
+            Name = name;
+            AgeGroup = ageGroup;
+            Bib = bib;
+            Bike = bike;
+            Run = run;
+            Validate(); // er værdierne i orden
+        }
+
+
+
+        /*
+         * 
+         * ToString
+         */
+        public override string ToString()
+        {
+            return $"{nameof(Name)}={Name}, {nameof(AgeGroup)}={AgeGroup}, {nameof(Bib)}={Bib}, {nameof(Bike)}={Bike}, {nameof(Run)}={Run}, Total={Total} ";
+        }
+
+    }
+}
---
+++ ClassDemoDuatlonLibTests/model/DuathleteTests.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ClassDemoDuatlonLib.model.Tests
+{
+    [TestClass()]
+    public class DuathleteTests
+    {
+        [TestMethod()]
+        [DataRow("1234")]
+        [DataRow("Et Meget langt navn")]
+        public void CheckNameAcceptTest(String name)
+        {
+            // arrange 
+            // act
+            // assert
+            Assert.IsTrue(Duathlete.CheckName(name));
+        }
+
+        [TestMethod()]
+        [DataRow(null)]
+        [DataRow("")]
+        [DataRow("123")]
+        [DataRow("          ")]
+        public void CheckNameFailTest(String name)
+        {
+            // arrange 
+            // act
+            // assert
+            Assert.ThrowsException<ArgumentException>(
+                () => Duathlete.CheckName(name)
+                );
+        }
+
+
+        [TestMethod()]
+        [DataRow(1)]
+        [DataRow(4)]
+        public void CheckAgeGroupAcceptTest(int group)
+        {
+            // arrange 
+            // act
+            // assert
+            Assert.IsTrue(Duathlete.CheckAgeGroup(group));
+        }
+
+        [TestMethod()]
+        [DataRow(0)]
+        [DataRow(5)]
+        [DataRow(-3)]
+        public void CheckAgeGroupFailTest(int group)
+        {
+            // arrange 
+            // act
+            // assert
+            Assert.ThrowsException<ArgumentException>(
+                () => Duathlete.CheckAgeGroup(group)
+                );
+        }
+
+        [TestMethod()]
+        public void CheckConstructorAcceptTest()
+        {
+            // arrange
+            String expectedName1 = "dummy";
+            String expectedName2 = "Peter";
+
+            // act
+            Duathlete dua1 = new Duathlete();
+            Duathlete dua2 = new Duathlete("Peter",4,4,4,4);
+
+            // assert
+            Assert.AreEqual(expectedName1, dua1.Name);
+            Assert.AreEqual(expectedName2, dua2.Name);
+        }
+
+        [TestMethod()]
+        [DataRow(null, 3)]
+        [DataRow("123", 3)]
+        [DataRow("1234", 0)]
+        public void CheckConstructorFailTest(String name, int ageGroup)
+        {
+            // arrange 
+            // act
+            // assert
+            Assert.ThrowsException<ArgumentException>(
+                () => new Duathlete(name, ageGroup, 4, 4, 4)
+                );
+        }
+
+    }
+}
